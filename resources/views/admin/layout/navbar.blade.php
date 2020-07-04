afc5f2aefacad25a5b688500510394a1df547288
Marium-sultana <mariumsultana073@gmail.com>
2020-07-04 19:48:21 +0600

    Blade template ready for admin

---
+++ resources/views/admin/layout/navbar.blade.php
@@ -0,0 +1,38 @@
+<div class="navbar">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+
+
+                    <!-- theme selector starts -->
+
+                    <!-- theme selector ends -->
+
+                    <!-- user dropdown starts -->
+                    <div class="btn-group pull-right" >
+                        <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-user"></i><span class="hidden-phone"> Marium Sultana</span>
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+
+                            <li><a href="http://localhost/irsbd/super_admin/logout">Logout</a></li>
+                        </ul>
+                    </div>
+                    <!-- user dropdown ends -->
+
+                    <div class="top-nav nav-collapse">
+                        <ul class="nav">
+                            <li><a href="http://localhost/irsbd/">Visit Site</a></li>
+                            <li>
+
+                            </li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'HomePageController@index');
+
+Route::group(['prefix' => 'admin'],function(){
+  Route::get('/', 'Admin\DashboardController@index');
+});
